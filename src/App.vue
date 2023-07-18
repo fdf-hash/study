c21f067c2208bf0d8c16b0ca19effb1a595bd5d4
付东飞 <fdf9758@163.com>
2023-07-18 16:27:24 +0800

    feat: piain

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,101 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <Layout>
+    <div>
+      <div style="margin: 1rem 0;">
+        <PiniaLogo />
+      </div>
+
+      <h2>Hello {{ user.name }}</h2>
+
+      <form @submit.prevent="addItemToCart" data-testid="add-items">
+        <input type="text" v-model="itemName" />
+        <button>Add</button>
+      </form>
+
+      <form @submit.prevent="buy">
+        <ul data-testid="items">
+          <li v-for="item in cart.items" :key="item.name">
+            {{ item.name }} ({{ item.amount }})
+            <button
+              @click="cart.removeItem(item.name)"
+              type="button"
+            >X</button>
+          </li>
+        </ul>
+
+        <button :disabled="!user.name">Buy</button>
+        <button
+          :disabled="!cart.items.length"
+          @click="clearCart"
+          type="button"
+          data-testid="clear"
+        >Clear the cart</button>
+      </form>
+    </div>
+  </Layout>
 </template>
 
+<script lang="ts">
+import Layout from './layout/default.vue'
+import PiniaLogo from './components/PiniaLogo.vue'
+
+import { defineComponent, ref } from 'vue'
+import { useUserStore } from './store/user'
+import { useCartStore } from './store/cart'
+
+export default defineComponent({
+  components: { Layout, PiniaLogo },
+
+  setup() {
+    const user = useUserStore()
+    const cart = useCartStore()
+
+    const itemName = ref('')
+
+    function addItemToCart() {
+      if (!itemName.value) return
+      cart.addItem(itemName.value)
+      itemName.value = ''
+    }
+
+    function clearCart() {
+      if (window.confirm('Are you sure you want to clear the cart?')) {
+        cart.rawItems = []
+      }
+    }
+
+    async function buy() {
+      const n = await cart.purchaseItems()
+
+      console.log(`Bought ${n} items`)
+
+      cart.rawItems = []
+    }
+
+    // @ts-ignore
+    window.stores = { user, cart }
+
+    return {
+      itemName,
+      addItemToCart,
+      cart,
+
+      user,
+      buy,
+      clearCart,
+    }
+  },
+})
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+img {
+  width: 200px;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+button,
+input {
+  margin-right: 0.5rem;
+  margin-bottom: 0.5rem;
 }
 </style>
