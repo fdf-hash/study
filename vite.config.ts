8ea90b587f3f85ebd883e1c1dc854ff509e0c643
付东飞 <fdf9758@163.com>
2023-07-18 15:50:29 +0800

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig, UserConfigExport } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig((context) => {
+  console.log(context)
+  const commonConfig: UserConfigExport = {
+    plugins: [
+      vue({
+        // 开启响应式语法糖
+        reactivityTransform: true
+      }),
+    ],
+    server: {
+      port: 3000,
+      open: false,
+      host: true, // 导出
+    }
+  }
+
+  return commonConfig
+})
