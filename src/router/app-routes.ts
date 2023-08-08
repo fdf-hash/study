e50f455bf2af41478188b4908a655d8091f5122f
付东飞 <fdf9758@163.com>
2023-08-08 17:19:34 +0800

    fix: update

--- src/router/app-routes.ts
+++ src/router/app-routes.ts
@@ -1,7 +1,7 @@
 import type { RouteRecordRaw } from 'vue-router'
-// import web from './apps/web'
-// import plan from './apps/plan'
-// import assets from './apps/assets'
+import web from './apps/web'
+import plan from './apps/plan'
+import assets from './apps/assets'
 import electricity from './apps/electricity'
 import work from './apps/work'
 import rsa from './apps/rsa'
@@ -11,9 +11,9 @@ import rsa from './apps/rsa'
 
 
 const appRoutes: RouteRecordRaw[] = [
-    // ...web,
-    // ...assets,
-    // ...plan,
+    ...web,
+    ...assets,
+    ...plan,
     ...work,
     ...rsa,
     ...electricity,
