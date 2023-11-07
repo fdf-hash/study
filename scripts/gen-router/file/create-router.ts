2f517eb862b1c7734d666a3df2f5f114c41b7055
付东飞 <fdf9758@163.com>
2023-11-07 18:29:05 +0800

    fix: 路由组装初始化完成

---
+++ scripts/gen-router/file/create-router.ts
@@ -0,0 +1,4 @@
+/**
+ * 创建  \my-vue-app\src\router\apps  创建路径下的所有路由文件
+ * 更新  \my-vue-app\src\router\app-routes.ts 更新该文件内容
+ */
---
+++ scripts/gen-router/file/delete-router.ts
@@ -0,0 +1,3 @@
+/**
+ * 删除  \my-vue-app\src\router\apps  删除此路径下的所有路由文件
+ */
