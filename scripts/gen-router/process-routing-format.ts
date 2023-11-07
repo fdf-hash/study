27be84fbb556e1a9990cb2d1d7ab2b3a111c7fee
付东飞 <fdf9758@163.com>
2023-11-07 17:04:29 +0800

    fix: 路由子级

--- scripts/gen-router/process-routing-format.ts
+++ scripts/gen-router/process-routing-format.ts
@@ -1,34 +1,108 @@
-import { log } from "console";
+import {log} from "console"
 
-export default function processFormat(routerData:any){
-    
+/**
+ * 路由类型参数
+ */
+interface RouteConfig {
+  path: string
+  props?: boolean
+  meta: {title: string}
+  component: any
+}
 
-    // routerData.forEach((item:any) => {
-    //     console.log(item.filePath);
-    // })
+export default function processFormat(routerData: any) {
+  // routerData.forEach((item:any) => {
+  //     console.log(item.filePath);
+  // })
 
-    firstLevelMenu(routerData)
+  firstLevelMenu(routerData)
 }
 /**
- * 判断路由是否为1级菜单 
-*/
-function firstLevelMenu(routerData:Record<string,any>[]) {
-    routerData.forEach(item => {
-        let itemSplitArr = item.match[1].split(' ')
-        // 如果 item.match 包含 path 为二级路由
-        if(itemSplitArr[2] != ''){
-            // 二级路由生成
-            secondaryRouting(item)
-        }
-       
-    })
-}
-
-function secondaryRouting(item:Record<string,any>){
-    log(item)
-    // 开始第一次组装路由
-
-    let obj = {
-        
+ * 判断路由是否为1级菜单
+ */
+function firstLevelMenu(routerData: Record<string, any>[]) {
+  routerData.forEach((item) => {
+    let itemSplitArr = item.match[1].split(" ")
+    // 如果 item.match 包含 path 为二级路由
+    if (itemSplitArr[2] != "") {
+      // 二级路由生成
+      assembleSecondaryRouting(item)
+    } else {
     }
+  })
 }
+
+function assembleSecondaryRouting(item: Record<string, any>) {
+  const {title, path} = parseRoute(item.match[0])
+  const {name} = pathParent(item.filePath)
+  let children: RouteConfig = {
+    path: `${name}/${path}`,
+    props: true,
+    meta: {title: title},
+    component: `() => import('${item.filePath.replace(/src\\/, "")}')`,
+  }
+
+  log(children)
+}
+/**
+ * 路由名称、路径
+ * @param routeStr
+ * @returns
+ */
+function parseRoute(routeStr: string): {title: string; path: string} {
+  const titleRegex = /title="([^"]*)"/
+  const match = routeStr.match(titleRegex)
+  const title = match ? match[1] : ""
+
+  const pathRegex = /path="([^"]*)"/
+  const pathMatch = routeStr.match(pathRegex)
+  const path = pathMatch ? pathMatch[1] : ""
+
+  return {title, path}
+}
+
+/**
+ * 子路由父级文件名称
+ * @param filePath
+ * @returns
+ */
+function pathParent(filePath: string): {name: string} {
+  const regex = /\\([^\\]+)\./
+  const match = filePath.match(regex)
+  const name = match ? match[1]! : ""
+  return {name}
+}
+/**
+ * 
+import type { RouteRecordRaw } from 'vue-router'
+import {layout} from 'layout'
+
+const assets: RouteRecordRaw[] = [
+    {
+        path: '/assets',
+        meta: { title: 'Assets网页' },
+        component: layout,
+        children:[
+            {
+                path:'index',
+                meta: { title: 'Assets网页' },
+                component:() => import('pages/assets/main.vue')
+            },
+            {
+                path: 'form/create',
+                props: true,
+                meta: { title: 'Assets网页(新增)' },
+                component: () => import('pages/assets/form.vue')
+            }, 
+            {
+                path: 'form/:id/view',
+                props: true,
+                meta: { title: 'Assets网页(查看)' },
+                component: () => import('pages/assets/form.vue')
+            },
+        ]
+    }
+]
+
+export default assets
+ */
