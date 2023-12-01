f748fa3cebf85487f6840bd2da11ea7fae2919fa
付东飞 <fdf9758@163.com>
2023-12-01 18:25:27 +0800

    fix: update

--- src/pages/views/assets/main.vue
+++ src/pages/views/assets/main.vue
@@ -9,11 +9,21 @@
 
 <script setup lang="ts">
 import {useRouter} from "vue-router"
+import CryptoJS from "crypto-js"
+const data = "要加密的数据"
+const key = "加密密钥"
+
+// 使用 AES 对数据进行加密
+const encryptedData = window.btoa(CryptoJS.AES.encrypt(data, key).toString())
+console.log(encryptedData);
+
+// 解密数据
+const decryptedData = CryptoJS.AES.decrypt(window.atob(encryptedData), key).toString(
+  CryptoJS.enc.Utf8
+)
+console.log(decryptedData)
 
 const routers = useRouter()
-
-import {downloadFileContext} from "unit"
-console.log(downloadFileContext)
 
 const path = () => {
   routers.push({path: "/assets/form/12313/view"})
