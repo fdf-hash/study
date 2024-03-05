e9a6b1fcbb42976c8370132a94d9072b88493067
付东飞 <fdf9758@163.com>
2024-03-05 15:16:38 +0800

    feat: gpt

--- src/gpt/openai-test.mjs
+++ src/gpt/openai-test.mjs
@@ -1,11 +1,11 @@
 import OpenAI from "openai";
 const openai = new OpenAI({
-    apiKey: ""
+    apiKey: "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"
 });
 async function main() {
     const completion = await openai.chat.completions.create({
         messages: [{ role: "system", content: "You are a helpful assistant." }],
-        model: "gpt-3.5-turbo",
+        model: "gpt-4"
     });
 
     console.log(completion.choices[0]);
